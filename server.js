f909cb7d62d39c166e38f909ffe883ffc55495d6
samjoshchen <sjoshchen@gmail.com>
2026-02-27 22:07:00 +0700

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => 
+{
+  res.send("CyberLearn Backend Running 🚀");
+});
+
+app.listen(5000, () => 
+{
+  console.log("Server running on port 5000");
+});
